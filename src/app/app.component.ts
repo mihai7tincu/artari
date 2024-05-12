0e67faf336e62b997c73b8cfb6899b94f59c0d0a
Mihai Tincu <mihai7tincu@gmail.com>
2024-05-12 19:27:09 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    FooterComponent,
+    NavBarComponent,
+    NgbModule,
+    RouterOutlet
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Artari Japonezi';
+}
