746060b8a4c0ecaa5a2be6bd8028fb9a670e7d15
Mihai Tincu <mihai7tincu@gmail.com>
2024-06-02 12:21:24 +0300

    shopping cart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
+import { FaqsComponent } from './faqs/faqs.component';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { PromotionsComponent } from './promotions/promotions.component';
-import { FaqsComponent } from './faqs/faqs.component';
+import { ShoppingCartComponent } from './shoppingc-cart/shopping-cart.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -12,5 +12,6 @@ export const routes: Routes = [
   { path: 'products', component: ProductsComponent },
   { path: 'promotions', component: PromotionsComponent },
   { path: 'faqs', component: FaqsComponent },
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent }
 ];
